128510b7a7d52bec608383fe353b7e9a7644c3a8
DmitryRs <68352090+Raskol-dm@users.noreply.github.com>
2020-07-15 23:34:14 +0300

    №1 лабораторная по ООП
    В текстовом файле указаны координаты точек фигуры ( в моём случае -> квадрат ) и рёбра , т.е. как эти точки соединяются между собой. Задача: построить фигуру , перемещение , масштабирования и поворот

---
+++ labnik_1/init_scene.cpp
@@ -0,0 +1,17 @@
+#include "init_scene.h"
+#include "errors.h"
+#include "draw.h"
+
+int init(graphics &a, QGraphicsView *gv){
+    a.scene = new QGraphicsScene(gv);
+    if (!a.scene)
+        return ERR_ALLOC;
+    return OK;
+}
+void clear(graphics &a){
+    a.scene->clear();
+}
+
+void set(QGraphicsView *gv, graphics &a){
+    gv->setScene(a.scene);
+}
